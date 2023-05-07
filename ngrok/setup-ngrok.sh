110f57ba38368a1300e5dcae696eb066ec66daf1
Faisal Ahmad <faisal.idreesi@gmail.com>
2023-05-07 20:38:04 +0500

    Removed old ngrok files

--- ngrok/setup-ngrok.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ./ngrok.zip
-sudo cp ngrok /usr/bin
-sudo cp ngrok.service /etc/systemd/system/
-systemctl enable ngrok.service
